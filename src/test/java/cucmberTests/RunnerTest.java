ba0564b99b50ced8ce89b5aae1bde5a444460a77
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2022-10-31 23:14:08 -0500

    remove testng

--- src/test/java/cucmberTests/RunnerTest.java
+++ src/test/java/cucmberTests/RunnerTest.java
@@ -5,10 +5,10 @@ import com.epam.jdi.light.elements.composite.WebPage;
 import cucmberTests.steps.User;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 
 import static com.epam.jdi.light.driver.WebDriverUtils.killAllSeleniumDrivers;
 import static com.epam.jdi.light.elements.init.UIFactory.$;
@@ -18,15 +18,19 @@ import static com.epam.jdi.light.elements.init.UIFactory.$;
     features = "classpath:features"
     , glue = {"com.epam.jdi.bdd", "cucmberTests"}
 )
-public class RunnerTest extends AbstractTestNGCucumberTests {
-    @BeforeClass
-    public static void setUp() {
+public class RunnerTest {
+
+    @BeforeClass()
+    public void setUp() {
+
         WebPage.openUrl("https://jdi-testing.github.io/jdi-light/");
         $("img#user-icon").click();
         new Form<User>().submit(new User().set(u -> { u.name = "Roman"; u.password = "Jdi1234"; }));
     }
-    @AfterClass
-    public static void shutDown() {
+
+
+    @AfterClass()
+    public void tearDownClass() {
         killAllSeleniumDrivers();
     }
 }
